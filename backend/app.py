20bedddb8b1c4e871c6076f370129af7472e4fdb
sydonaeengland <sydonaeengland@gmail.com>
2026-02-28 14:56:06 -0500

    Update gitignore and remove venv

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,44 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from sqlalchemy import text
 
-app = Flask(__name__)
+load_dotenv(dotenv_path=Path(__file__).with_name(".env"))
 
-@app.route("/health")
-def health():
-    return jsonify({"status": "DLRSJAM backend running"})
+db = SQLAlchemy()
+
+print("DB_PORT =", os.getenv("DB_PORT"))
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"postgresql://{os.getenv('DB_USER')}:"
+        f"{os.getenv('DB_PASSWORD')}@"
+        f"{os.getenv('DB_HOST')}:"
+        f"{os.getenv('DB_PORT')}/"
+        f"{os.getenv('DB_NAME')}"
+    )
+
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    @app.route("/health")
+    def health():
+        return jsonify({"status": "DLRSJAM backend running"})
+
+    @app.route("/db-check")
+    def db_check():
+        db.session.execute(text("SELECT 1"))
+        return jsonify({"database": "connected"})
+
+    return app
+
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
